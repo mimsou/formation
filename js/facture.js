0eee879e14841aa5af05fb2d23c80619a3b29a20
souheyeb <souheyeb@gmail.com>
2021-03-08 17:57:52 +0000

    jquery

--- js/facture.js
+++ js/facture.js
@@ -1,14 +1,17 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  var element = document.getElementsByClassName("submit_detail")[0];
-  element.addEventListener(
-    "click",
-    function (e) {
-      var detailform = {};
-      detailform.desingnation = document.getElementById("designation").value;
-      detailform.id = document.getElementById("qte").value;
-      detailform.prix = document.getElementById("prix").value;
-       var html = ""
-    },
-    false
-  );
-});
+$(function(){
+
+
+  $("#designation").on("focus",function(){
+     
+  })
+
+    $(".submit_detail").click(function(){
+      $("#article-form input").each(function(){
+          
+      })
+    })
+
+})
+
+
+ 
